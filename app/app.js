cbb37747ccfda9602fc53c1c92a3bafea31be64c
Tony Mackay <tony@viewmodel.net>
2020-06-09 11:59:08 +0100

    fail lint test

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const yahoo = require('yahoo-finance');
 const app = express()
 const port = 8080;
 
-app.get('/quote', async (req, res) => {
+app.get('/quote', async (req, res, fail) => {
     var response = []
     const symbol = req.query.symbol;
 
